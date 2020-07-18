55a66ebd3033a29723098754595df96e26dbb96d
drazioti <drazioti@gmail.com>
2020-07-19 01:27:16 +0300

    add  : papadopoulos.pdf

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 # how to use it: $sh script.sh
 # Author : K.Draziotis (2019)
 
-openssl genrsa -out example.key 1024    # generate the RSA public key with modulus of specific number of bits
+openssl genrsa -out example.key 512    # generate the RSA public key with modulus of specific number of bits
 openssl rsa -in example.key -noout -modulus > hex.out 
 awk -F '=' '{print $2}' hex.out > dec.out
 sed -i '1s/^/ibase=16;\n/' dec.out
